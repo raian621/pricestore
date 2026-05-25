ff88c3042d11b410f1bae2b206880f8264b48dbf
Bell, Ryan <ryan.bell62101@gmail.com>
2026-05-25 12:58:23 -0700

    fix: use .Get to get the string from the password secret string in DB connection string util function

--- src/db/client.go
+++ src/db/client.go
@@ -10,6 +10,6 @@ func CreateDbConnString(
 	user string, password secret.Secret[string], host string, port uint,
 	name string,
 ) secret.Secret[string] {
-	return secret.Make(fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, password,
-		host, port, name))
+	return secret.Make(fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user,
+		password.Get(), host, port, name))
 }
